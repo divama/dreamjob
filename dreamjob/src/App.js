fdc22ef7dbceb2d9f745e92eb5ec865fd973b3bc
Diantha <dianthavanmarion@gmail.com>
2017-08-15 21:55:36 +0200

    Remove app.css and default header

--- dreamjob/src/App.js
+++ dreamjob/src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <a href="https://divama.github.io/Me/">https://divama.github.io/Me</a>
-          <a href="https://github.com/divama/Me/projects/1">
-            https://github.com/divama/Me/projects/1
-          </a>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <a href="https://divama.github.io/Me/">https://divama.github.io/Me</a>
+        <a href="https://github.com/divama/Me/projects/1">
+          https://github.com/divama/Me/projects/1
+        </a>
 
         <Grid columns={2} stackable>
           <Grid.Row>
