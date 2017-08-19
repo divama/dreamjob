719bc22e84220d5c261b31d7e3ba5776566de7b6
Diantha <dianthavanmarion@gmail.com>
2017-08-19 16:33:24 +0200

    Education component

--- dreamjob/src/myComponents/Education.js
+++ dreamjob/src/myComponents/Education.js
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { List, Icon, Header } from 'semantic-ui-react';
 
-class Education extends Component {
-  render() {
-    return <div>Education info</div>;
-  }
-}
+const Education = () =>
+  <div>
+    <Header as="h3" dividing>
+      Education
+    </Header>
+
+    <List divided relaxed>
+      <List.Item>
+        <List.Icon name="folder" />
+        <List.Content>
+          <List.Header>2000 - 2017</List.Header>
+          <List.List>
+            <List.Item>
+              <List.Content>
+                <List.Header>Function at company</List.Header>
+                <List.Description>Description</List.Description>
+              </List.Content>
+            </List.Item>
+          </List.List>
+        </List.Content>
+      </List.Item>
+
+      <List.Item>
+        <List.Icon name="folder" />
+        <List.Content>
+          <List.Header>2000 - 2017</List.Header>
+          <List.List>
+            <List.Item>
+              <List.Content>
+                <List.Header>Function at company</List.Header>
+                <List.Description>Description</List.Description>
+              </List.Content>
+            </List.Item>
+          </List.List>
+        </List.Content>
+      </List.Item>
+    </List>
+  </div>;
 
 export default Education;
