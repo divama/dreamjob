b42a2d979b61ee35615129409d633feb01b3f58a
divama <30927031+divama@users.noreply.github.com>
2017-08-19 14:48:12 +0200

    Feature/mockup (#21)
    * rename App to Resume. added a folder for myComponents
    
    * Change App to Resumer as an oveviewpage in pages folder. Add contact component in myComponents and load it in Resumer
    
    * Added Name component to Resume
    
    * Added footer. extended the Resumer with the needed components. Created empty components
    
    * Created component for Footer
    
    * Moved Photo component from Resumer to Footer
    
    * Removed components folder as we grab them from the node modules.

---
+++ dreamjob/src/pages/Resume.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+
+import Name from './../myComponents/Name';
+import Contact from './../myComponents/Contact';
+import Employment from './../myComponents/Employment';
+import Education from './../myComponents/Education';
+import Skills from './../myComponents/Skills';
+import Footer from './../myComponents/Footer';
+
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <Grid columns={2} stackable>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Segment>
+                <Name />
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <Segment>
+                <Contact />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Segment>
+                <Employment />
+              </Segment>
+              <Segment>
+                <Education />
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <Segment>
+                <Skills />
+              </Segment>
+              <Segment>
+                <Skills />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Footer />
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Resume;
