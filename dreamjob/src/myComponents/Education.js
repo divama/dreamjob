5798488a5b720ff3cbea15a161f6bc49daa35770
Diantha <dianthavanmarion@gmail.com>
2017-08-26 16:15:32 +0200

    sec activities, removed list

--- dreamjob/src/myComponents/Education.js
+++ dreamjob/src/myComponents/Education.js
@@ -3,49 +3,55 @@ import { Header, Accordion, Icon } from 'semantic-ui-react';
 
 const Education = () =>
   <div>
-    <Header as="h3" dividing>
-      Education
-    </Header>
-
     <Accordion>
       <Accordion.Title>
-        <Icon name="student" /> 2003 - 2006 <br /> Ingenieur: Mayor
-        Communication and Multimedia Design @ Haagse Hogeschool/TH Rijswijk
-        <Icon name="dropdown" />
+        <Header as="h3">
+          Education <Icon name="dropdown" />
+        </Header>
       </Accordion.Title>
       <Accordion.Content>
-        Competenties: Projectmatig werken, adviseren, resultaatgericht werken,
-        methodisch werken, communicatief functioneren, ontwikkelingsvermogen,
-        maatschappelijke verantwoordelijkheid, helikopterblik, thrill, creërend
-        vermogen, abstractievermogen, marktgevoel, organisatiegerichtheid,
-        affiniteit met kenniswerk, zelfstandigheid, leiding geven. Minor
-        Informatica Competenties: Opstellen objectmodel, opstellen
-        gegevensmodel, opstellen implementatie-gegevensmodel, ontwerpen
-        functionele specificaties, ontwerpen applicatiestructuur, ontwerpen
-        programmaspecificaties, programmeren, ontwerpen mens/machine interface,
-        maken testplan, uitvoeren test.
-      </Accordion.Content>
+        <Accordion>
+          <Accordion.Title>
+            <Icon name="student" /> 2003 - 2006 <br /> Ingenieur: Mayor
+            Communication and Multimedia Design @ Haagse Hogeschool/TH Rijswijk
+            <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>
+            Competenties: Projectmatig werken, adviseren, resultaatgericht
+            werken, methodisch werken, communicatief functioneren,
+            ontwikkelingsvermogen, maatschappelijke verantwoordelijkheid,
+            helikopterblik, thrill, creërend vermogen, abstractievermogen,
+            marktgevoel, organisatiegerichtheid, affiniteit met kenniswerk,
+            zelfstandigheid, leiding geven. Minor Informatica Competenties:
+            Opstellen objectmodel, opstellen gegevensmodel, opstellen
+            implementatie-gegevensmodel, ontwerpen functionele specificaties,
+            ontwerpen applicatiestructuur, ontwerpen programmaspecificaties,
+            programmeren, ontwerpen mens/machine interface, maken testplan,
+            uitvoeren test.
+          </Accordion.Content>
 
-      <p>
-        <br />
-        <Icon name="student" /> 2003 - 2006 <br /> Ingenieur: Minor Informatica
-        @ Haagse Hogeschool/TH Rijswijk
-      </p>
+          <p>
+            <br />
+            <Icon name="student" /> 2003 - 2006 <br /> Ingenieur: Minor
+            Informatica @ Haagse Hogeschool/TH Rijswijk
+          </p>
 
-      <Accordion.Title>
-        <Icon name="student" /> 1999 - 2003 <br /> Multimedia Vormgeven @
-        Grafisch Lyceum Rotterdam
-        <Icon name="dropdown" />
-      </Accordion.Title>
-      <Accordion.Content>
-        In de opleiding leer je werken met kleur, vlakverdeling, lettertypes,
-        illustraties, foto’s en bewegend beeld. Je leert hoe je verschillende
-        teken- en ontwerptechnieken toepast én hoe je alles uitwerkt op de pc of
-        Mac. Een groot deel van de tijd werk je met de computer en de
-        belangrijkste softwarepakketten van Adobe, zoals Illustrator en
-        Photoshop. Daarnaast leer je werken met software als 3d Studio Max en
-        Dreamweaver. Ook krijg je vakken op het gebied van kunst,
-        conceptontwikkeling, en ondernemersvaardigheden.
+          <Accordion.Title>
+            <Icon name="student" /> 1999 - 2003 <br /> Multimedia Vormgeven @
+            Grafisch Lyceum Rotterdam
+            <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>
+            In de opleiding leer je werken met kleur, vlakverdeling,
+            lettertypes, illustraties, foto’s en bewegend beeld. Je leert hoe je
+            verschillende teken- en ontwerptechnieken toepast én hoe je alles
+            uitwerkt op de pc of Mac. Een groot deel van de tijd werk je met de
+            computer en de belangrijkste softwarepakketten van Adobe, zoals
+            Illustrator en Photoshop. Daarnaast leer je werken met software als
+            3d Studio Max en Dreamweaver. Ook krijg je vakken op het gebied van
+            kunst, conceptontwikkeling, en ondernemersvaardigheden.
+          </Accordion.Content>
+        </Accordion>
       </Accordion.Content>
     </Accordion>
   </div>;
--- dreamjob/src/myComponents/SecActivities.js
+++ dreamjob/src/myComponents/SecActivities.js
@@ -1,108 +1,53 @@
 import React from 'react';
-import { List, Header, Accordion, Icon } from 'semantic-ui-react';
+import { Header, Accordion, Icon } from 'semantic-ui-react';
 
 const SecActivities = () =>
   <div>
     <Accordion>
       <Accordion.Title>
-        <Header as="h3" dividing>
+        <Header as="h3">
           Secondary Activities <Icon name="dropdown" />
         </Header>
       </Accordion.Title>
       <Accordion.Content>
-        <List divided relaxed>
-          <List.Item>
-            <List.Icon name="book" />
-            <List.Content>
-              <List.Header>2010</List.Header>
-              <Accordion>
-                <Accordion.Title>
-                  <List.Description>
-                    MOSS/WCM SP07 website UMC st.Radboud uitgeroepen tot beste
-                    ziekenhuiswebsite van nederland! @ Tamtam
-                    <Icon name="dropdown" />
-                  </List.Description>
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List.Description>
-                    (www.umcn.nl) volgens www.zorgwebmonitor.nl
-                  </List.Description>
-                </Accordion.Content>
-              </Accordion>
-            </List.Content>
-          </List.Item>
+        <Accordion>
+          <Accordion.Title>
+            <Icon name="book" /> 2010 <br />MOSS/WCM SP07 website UMC st.Radboud
+            uitgeroepen tot beste ziekenhuiswebsite van nederland! @ Tamtam
+            <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>
+            (www.umcn.nl) volgens www.zorgwebmonitor.nl
+          </Accordion.Content>
 
-          <List.Item>
-            <List.Icon name="book" />
-            <List.Content>
-              <List.Header>2009</List.Header>
-              <Accordion>
-                <Accordion.Title>
-                  <List.Description>
-                    SharePointMagazine publicaties @ Tamtam
-                    <Icon name="dropdown" />
-                  </List.Description>
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List.Description>
-                    Verschillende publicaties geschreven over SharePoint’s 2007
-                    Branding Limitations. www.sharepointmagazine.net
-                  </List.Description>
-                </Accordion.Content>
-              </Accordion>
-            </List.Content>
-          </List.Item>
+          <Accordion.Title>
+            <Icon name="book" /> 2009 <br />SharePointMagazine publicaties @
+            Tamtam
+            <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>
+            Verschillende publicaties geschreven over SharePoint’s 2007 Branding
+            Limitations. www.sharepointmagazine.net
+          </Accordion.Content>
 
-          <List.Item>
-            <List.Icon name="book" />
-            <List.Content>
-              <List.Header>2009</List.Header>
-              <Accordion>
-                <Accordion.Title>
-                  <List.Description>
-                    Blog over front-end development voor SharePoint 2007 @
-                    Tamtam <Icon name="dropdown" />
-                  </List.Description>
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List.Description>
-                    <List.Description>www.tamtam.nl</List.Description>
-                  </List.Description>
-                </Accordion.Content>
-              </Accordion>
-            </List.Content>
-          </List.Item>
+          <Accordion.Title>
+            <Icon name="book" /> 2009 <br />Blog over front-end development voor
+            SharePoint 2007 @ Tamtam <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>www.tamtam.nl</Accordion.Content>
 
-          <List.Item>
-            <List.Icon name="book" />
-            <List.Content>
-              <List.Header>2006</List.Header>
-              <List.Description>
-                Hands-on training for Flash @ Haagse Hogeschool/TH Rijswijk
-              </List.Description>
-            </List.Content>
-          </List.Item>
+          <p>
+            <br />
+            <Icon name="book" />2006 <br />Hands-on training for Flash @ Haagse
+            Hogeschool/TH Rijswijk
+          </p>
 
-          <List.Item>
-            <List.Icon name="book" />
-            <List.Content>
-              <List.Header>2004 - 2005</List.Header>
-              <Accordion>
-                <Accordion.Title>
-                  <List.Description>
-                    Kampleider introductiekamp informatica @ Haag Uit.
-                    <Icon name="dropdown" />
-                  </List.Description>
-                </Accordion.Title>
-                <Accordion.Content>
-                  <List.Description>
-                    <List.Description>www.haaguit.com </List.Description>
-                  </List.Description>
-                </Accordion.Content>
-              </Accordion>
-            </List.Content>
-          </List.Item>
-        </List>
+          <Accordion.Title>
+            <Icon name="book" /> 2004 - 2005 <br />Kampleider introductiekamp
+            informatica @ Haag Uit. <Icon name="dropdown" />
+          </Accordion.Title>
+          <Accordion.Content>www.haaguit.com</Accordion.Content>
+        </Accordion>
       </Accordion.Content>
     </Accordion>
   </div>;
