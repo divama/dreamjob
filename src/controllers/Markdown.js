3af41ffa16a2018bc4f4bec7224436ca01372ba5
Diantha <dianthavanmarion@gmail.com>
2017-09-01 23:33:35 +0200

    moved src folder

---
+++ src/controllers/Markdown.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+class Markdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contents: ''
+    };
+  }
+
+  componentDidMount() {
+    this.fetchContent();
+  }
+
+  fetchContent() {
+    fetch(this.props.src)
+      .then(response => response.text())
+      .then(contents => this.setState({ contents }));
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.src !== this.props.src) {
+      this.fetchContent();
+    }
+  }
+
+  render() {
+    return <ReactMarkdown source={this.state.contents} />;
+  }
+}
+
+export default Markdown;
