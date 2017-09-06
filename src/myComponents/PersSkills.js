415f5129db741579bf6aba1bb254890f64ba37f6
Diantha van Marion <diantha.van.marion@exact.com>
2017-09-06 11:18:28 +0200

    Orders the stars

--- src/myComponents/PersSkills.js
+++ src/myComponents/PersSkills.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Rating, Header, List, Accordion, Icon } from 'semantic-ui-react';
 
-const PersSkills = () =>
+const PersSkills = () => (
   <div>
     <Accordion>
       <Accordion.Title>
@@ -11,14 +11,26 @@ const PersSkills = () =>
       </Accordion.Title>
       <Accordion.Content>
         <List>
+          {/* 5 stars */}
           <List.Item>
-            Creativity<Rating
-              defaultRating={4}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
+            Organizing<Rating defaultRating={5} maxRating={5} disabled className="right floated" />
           </List.Item>
+
+          {/* 4 stars */}
+          <List.Item>
+            Creativity<Rating defaultRating={4} maxRating={5} disabled className="right floated" />
+          </List.Item>
+          <List.Item>
+            Team player<Rating defaultRating={4} maxRating={5} disabled className="right floated" />
+          </List.Item>
+          <List.Item>
+            Barista<Rating defaultRating={4} maxRating={5} disabled className="right floated" />
+          </List.Item>
+          <List.Item>
+            EHBO<Rating defaultRating={4} maxRating={5} disabled className="right floated" />
+          </List.Item>
+
+          {/* 3 stars */}
           <List.Item>
             Communication<Rating
               defaultRating={3}
@@ -27,45 +39,10 @@ const PersSkills = () =>
               className="right floated"
             />
           </List.Item>
+
+          {/* 2 stars */}
           <List.Item>
-            Organizing<Rating
-              defaultRating={5}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
-          </List.Item>
-          <List.Item>
-            Team player<Rating
-              defaultRating={4}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
-          </List.Item>
-          <List.Item>
-            Presenting<Rating
-              defaultRating={2}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
-          </List.Item>
-          <List.Item>
-            Barista<Rating
-              defaultRating={4}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
-          </List.Item>
-          <List.Item>
-            EHBO<Rating
-              defaultRating={4}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
+            Presenting<Rating defaultRating={2} maxRating={5} disabled className="right floated" />
           </List.Item>
         </List>
 
@@ -74,24 +51,15 @@ const PersSkills = () =>
             <Header as="h4">Languages</Header>
           </List.Item>
           <List.Item>
-            Dutch<Rating
-              defaultRating={5}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
+            Dutch<Rating defaultRating={5} maxRating={5} disabled className="right floated" />
           </List.Item>
           <List.Item>
-            English<Rating
-              defaultRating={5}
-              maxRating={5}
-              disabled
-              className="right floated"
-            />
+            English<Rating defaultRating={5} maxRating={5} disabled className="right floated" />
           </List.Item>
         </List>
       </Accordion.Content>
     </Accordion>
-  </div>;
+  </div>
+);
 
 export default PersSkills;
