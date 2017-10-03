e33a8f08e05dc237fb3aa1728aeedd5d286c4d5a
Diantha van Marion <diantha.van.marion@exact.com>
2017-10-03 16:13:25 +0200

    Revert "Very first version of page setup with routing"
    This reverts commit 6a8b081e1b0709f7a8a94380bb8c709661290be6.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
 
 // Import css
 import './semantic.min.css';
 
-const App = () => (
-  <div>
-    App. <Link to="/EmploymentPage">EmploymentPage</Link>
-  </div>
-);
+// Import Components
+import Resume from './pages/Resume';
 
-const EmploymentPage = () => (
-  <div>
-    EmploymentPage. <Link to="/">App</Link>
-  </div>
-);
-
-ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={App} />
-      <Route path="/EmploymentPage" component={EmploymentPage} />
-    </div>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Resume />, document.getElementById('root'));
+registerServiceWorker();
