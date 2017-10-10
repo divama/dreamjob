71d2304b36f08a1de44b736f373a227f8d7c40dd
Diantha <dianthavanmarion@gmail.com>
2017-10-10 23:14:29 +0200

    Removing testfiles as hey are nw not used

--- src/myComponents/Training.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Training from './Training';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Training />, div);
-});
